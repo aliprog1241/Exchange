a6a119adf11b5db943bbd13fe229090c429f87ee
Ali Miri <mirio3369@gmail.com>
2025-07-10 15:34:46 +0330

    create a code for notificaion

---
+++ config.py
@@ -0,0 +1,13 @@
+BASE_PATH = 'https://data.fixer.io/api/latest?access_key='
+
+API_KEY = 'b4e56a512b6cc81723b2135671694f3f'
+
+url = BASE_PATH + API_KEY 
+
+EMAIL_RECEIVER = 'miri03369@gmail.com'
+
+rulse = {
+    'archive': True,
+    'send_mail': True,
+    'preferred' : ['BTC', 'IRR', "USD", "CAD" ]
+}
--- main.py
+++ main.py
@@ -1,1 +1,46 @@
 import requests
+import json
+import os
+
+from config import url,rulse
+
+#for API and response
+
+def get_rates():  
+    response = requests.get(url)
+    if response.status_code == 200 :
+        return json.loads(response.text)
+    return None
+    
+#for run json code
+
+def archive(filename, rates):
+    if not os.path.exists("archive"):
+        os.makedirs("archive")
+
+    with open(f'archive/{filename}.json', 'w') as f:
+        f.write(json.dumps(rates, indent=4))
+
+#for email 
+
+def send_mail(timestamp, rates):
+    subject = f'{timestamp} rates'
+    if rulse['preferred'] is not None :
+        tmp = dict()
+        for exc in rulse['preferred'] :
+            tmp[exc] = rates[exc]
+        rates = tmp
+
+
+    text  = json.dumps(rates)
+
+
+    
+
+
+if __name__ == "__main__" :
+    response = get_rates()
+    if rulse['archive']:
+        archive(response['timestamp'], response['rates'])
+    if rulse['send_mail']:
+        archive(response['timestamp'], response['rates'])
