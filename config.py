3c8505b83a0abaa6c15a15834824a50cc713aada
Ali Miri <mirio3369@gmail.com>
2025-07-11 14:27:24 +0330

    Add notification and send massage for user

--- config.py
+++ config.py
@@ -6,8 +6,30 @@ url = BASE_PATH + API_KEY
 
 EMAIL_RECEIVER = 'miri03369@gmail.com'
 
-rulse = {
+
+
+"""rulse = {
     'archive': True,
     'send_mail': True,
     'preferred' : ['BTC', 'IRR', "USD", "CAD" ]
+}
+"""
+rulse  = {
+    'archive' : True,
+    'email':{
+        'send_mail' : True,
+        'preferred' : ['BTC', 'IRR', "USD", "CAD" ],
+    },
+    'notification':{
+        'enable' : True,
+        'receiver' : '09199983035',
+        'preferred' :{
+            'BTC' : {'min':9.852039e-06, 'max':9.932039e-06},
+            'IRR':{'min':49231.957393 , 'max':49244.957393},
+
+        }
+
+        
+    }
+
 }
--- main.py
+++ main.py
@@ -34,13 +34,29 @@ def send_mail(timestamp, rates):
 
     text  = json.dumps(rates)
 
+# add  notification
 
-    
+def chek_notify_rules(rates):
+    preferred = rulse['notification'],['preferred']
+    msg =''
+    for exc in preferred:
+        if rates[exc] <= preferred[exc]['min']:
+            msg += f'{exc}reached nin:{rates[exc]}'
+        if rates[exc] >= preferred[exc]['max']:
+            msg += f'{exc}reached max:{rates[exc]}'
+    return msg
+
+def send_notification():
+    pass
 
 
 if __name__ == "__main__" :
     response = get_rates()
     if rulse['archive']:
         archive(response['timestamp'], response['rates'])
-    if rulse['send_mail']:
+    if rulse['email']:
         archive(response['timestamp'], response['rates'])
+    if rulse['notification']['enable']:
+        notificatin_msg = chek_notify_rules(response['rates'])
+        if notificatin_msg:
+            send_notification(notificatin_msg)
